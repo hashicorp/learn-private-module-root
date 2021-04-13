d95801e4533b92e14bd1692adbc2f5d8763875e0
dasc345 <dasc345@gmail.com>
2021-04-13 14:51:19 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,9 +3,10 @@ provider "aws" {
 }
 
 module "s3-webapp" {
-  source  = "app.terraform.io/hashicorp-learn/s3-webapp/aws"
-  name        = var.name
-  region = var.region
-  prefix = var.prefix
+  source  = "app.terraform.io/dasc345/bart-s3-webapp/aws"
   version = "1.0.0"
+  source  = "app.terraform.io/hashicorp-learn/s3-webapp/aws"
+  name    = var.name
+  region  = var.region
+  prefix  = var.prefix
 }
