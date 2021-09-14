3978b32e55bcdc87816a6011c229e62a2912b447
ayahmuhamed <88331884+ayahmuhamed@users.noreply.github.com>
2021-09-14 02:54:50 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "s3-webapp" {
-  source  = "app.terraform.io/hashicorp-learn/s3-webapp/aws"
+  source  = "app.terraform.io/app/hashicorpaya/workspaces/learn-private-module-root"
   name        = var.name
   region = var.region
   prefix = var.prefix
