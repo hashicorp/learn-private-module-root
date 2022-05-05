d8bd4e0ee74ef1be600ed2fbc14485da3b22a692
dasc345 <dasc345@gmail.com>
2022-05-05 16:46:29 +0200

    Change web config resources

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 module "s3-webapp" {
   source  = "app.terraform.io/dasc345/bart-s3-webapp/aws"
-  version = "1.0.0"
+  version = "1.0.1"
   name    = var.name
   region  = var.region
   prefix  = var.prefix
