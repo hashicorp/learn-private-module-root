d9cfe7498b3c18a11662f611a1f4469b3a5edf7a
Morgan Clark <89749642+mclark0814@users.noreply.github.com>
2022-07-05 18:06:39 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "s3-webapp" {
-  source  = "app.terraform.io/hashicorp-learn/s3-webapp/aws"
+  source  = "app.terraform.io/mclark0814/s3-webapp/aws"
   name        = var.name
   region = var.region
   prefix = var.prefix
