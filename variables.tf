9d4222ee28b8ca0fa7932f76180d543f69359967
dbeleva-af <dbeleva_af@abv.bg>
2023-07-04 09:48:09 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,14 +1,17 @@
 variable "region" {
   description = "This is the cloud hosting region where your webapp will be deployed."
   type        = string
+  default     = "us-west-2"
 }
 
 variable "prefix" {
   description = "This is the environment your webapp will be prefixed with. dev, qa, or prod."
   type        = string
+  default     = "dev"
 }
 
 variable "name" {
   description = "Your name to attach to the webapp address."
   type        = string
+  default     = "dv"
 }
