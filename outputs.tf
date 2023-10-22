a1d9cfe075b87a97ab4010f3e6e586716dbacbf2
Jens <65734789+jdrepo@users.noreply.github.com>
2023-10-22 17:20:30 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,7 @@ output "website_domain" {
   description = "HTTP domain for example website."
   value       = module.s3-webapp.domain
 }
+output "website_url" {
+  description = "URL for example website."
+  value       = module.s3-webapp.url
+}
