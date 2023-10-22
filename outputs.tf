b21466b2d2e81a0f975b7e7f608f427b1de18e31
Jens <65734789+jdrepo@users.noreply.github.com>
2023-10-22 16:41:45 +0200

    add domain to output

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,7 @@ output "website_endpoint" {
   description = "HTTP endpoint for example website."
   value       = module.s3-webapp.endpoint
 }
+output "website_domain" {
+  description = "HTTP domain for example website."
+  value       = module.s3-webapp.domain
+}
